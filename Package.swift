297486a4b4c679193672f6005dd83b0013f5f64c
snapplay <devman@snapplay.io>
2025-09-02 16:36:11 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "s2offerwall",
-            url: "https://raw.githubusercontent.com/snapplay-io/ios_offerwall_sdk/main/sdk/s2offerwall.xcframework_v1.0.10.zip",
-            checksum: "b437d51100533c1b8b035920d621fec4de22d712abe90bc7e57387c03201cb12"
+            url: "https://raw.githubusercontent.com/snapplay-io/ios_offerwall_sdk/main/sdk/s2offerwall.xcframework_v1.0.11.zip",
+            checksum: "4434245498b431c653dec8176c34ab25ec980042673c208b53d0f032ba10b9c1"
         )
     ]
 )
