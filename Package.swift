0bd62d6752cf4fb9b0652429aa366e23cad69131
snapplay <devman@snapplay.io>
2025-09-02 09:35:50 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "s2offerwall",
+    platforms: [
+        .iOS(.v16)   // 최소 지원 버전
+    ],
+    products: [
+        .library(
+            name: "s2offerwall",
+            targets: ["s2offerwall"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "s2offerwall",
+            url: "https://raw.githubusercontent.com/snapplay-io/ios_offerwall_sdk/main/sdk/s2offerwall.xcframework_v1.06.zip",
+            checksum: "1e0207fa7d242606b3f91f58d1dff3c5db6624986f6413519f602230c83f2668"
+        )
+    ]
+)
